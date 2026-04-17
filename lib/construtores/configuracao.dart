5787ab28e09d577030bca9d8037032640490df07
itajacy TI02 <itajacy@bsd.com.br>
2026-04-17 10:23:25 -0300

    construtor privado

---
+++ lib/construtores/configuracao.dart
@@ -0,0 +1,6 @@
+class Configuracao {
+  static String token = 'abc';
+
+  //  construtor privado  basta colocar o _, como se fosse um nomeado
+  Configuracao._();
+}
---
+++ lib/construtores/construtor_privado.dart
@@ -0,0 +1,11 @@
+import 'package:oop_dart/construtores/configuracao.dart';
+
+void main(List<String> args) {
+
+  // final config = Configuracao._();  // nao é possível acessar pq  o construtor da classe é privado
+  // final config = Configuracao();
+
+  // config.token;
+  
+  print(Configuracao.token);
+}
